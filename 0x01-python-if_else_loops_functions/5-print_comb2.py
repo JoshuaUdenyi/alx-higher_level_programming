6c7aa4f3f546bd986fcad53401c5687a21237a59
JoshuaUdenyi <udenyijoshua95@gmail.com>
2022-09-22 11:34:04 -0700

    print_comb

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#Author Udenyi Joshua
+
+"""A program that prints numbers from 0 to 99."""
+for num in range(0, 100):
+    if num == 99:
+        print("{}".format(num))
+    else:
+        print("{:02}".format(num), end=", ")
