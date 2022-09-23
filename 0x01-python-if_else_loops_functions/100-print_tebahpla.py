e50f10c8c274f3eb7893df2dac577c1b4d6aa61b
JoshuaUdenyi <udenyijoshua95@gmail.com>
2022-09-22 17:46:02 -0700

    fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,6 @@
 # Author - Udenyi Joshua
 
 for ascii_num in range(122, 96, -1):
-	if ascii_num % 2 == 1:
-	    ascii_num = ascii_num - 32
-	    print("{:c}".format(ascii_num), end='')
+    if ascii_num % 2 == 1:
+        ascii_num = ascii_num - 32
+    print("{:c}".format(ascii_num), end='')
